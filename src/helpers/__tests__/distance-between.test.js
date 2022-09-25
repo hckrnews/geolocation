edf370eb874dc2d5ac835512d91da4ea96b448a8
Pieter Wigboldus <w3n@w3news.org>
2022-09-25 10:20:07 +0200

    Add another test

--- src/helpers/__tests__/distance-between.test.js
+++ src/helpers/__tests__/distance-between.test.js
@@ -3,7 +3,7 @@ import assert from 'node:assert'
 import distanceBetween from '../distance-between.js'
 
 test('Test the distance between', async (t) => {
-  await t.test('It should return the distance between 2 locations', () => {
+  await t.test('It should return the distance between 2 locations, moved 1 longitude', () => {
     const coords = {
       latitude: 51.5074,
       longitude: 0.1278
@@ -15,6 +15,21 @@ test('Test the distance between', async (t) => {
 
     const result = distanceBetween({ coords, location })
     const expected = 69208.6913674116
+
+    assert.strictEqual(result, expected)
+  })
+  await t.test('It should return the distance between 2 locations, moved 1 latitude', () => {
+    const coords = {
+      latitude: 51.5074,
+      longitude: 0.1278
+    }
+    const location = {
+      latitude: 52.5074,
+      longitude: 0.1278
+    }
+
+    const result = distanceBetween({ coords, location })
+    const expected = 111194.92664455874
 
     assert.strictEqual(result, expected)
   })
