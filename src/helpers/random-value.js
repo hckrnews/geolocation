4196eacaa30c0ea594907d439a69a0d476ee8c7c
Pieter Wigboldus <w3n@w3news.org>
2022-09-25 10:35:06 +0200

    Add docblocks

--- src/helpers/random-value.js
+++ src/helpers/random-value.js
@@ -1,3 +1,11 @@
 import { randomInt } from 'node:crypto'
 
+/**
+ * Get a random value between 2 values
+ *
+ * @param {number} min
+ * @param {number} max
+ *
+ * @returns {number}
+ */
 export default (min, max) => randomInt(min, max + 1)
