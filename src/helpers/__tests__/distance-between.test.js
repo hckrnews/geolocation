6b5901c8f7d6659625a22bc3d2ef0ed6edb1d850
Pieter Wigboldus <w3n@w3news.org>
2022-09-25 10:24:49 +0200

    More tests for the distance

--- src/helpers/__tests__/distance-between.test.js
+++ src/helpers/__tests__/distance-between.test.js
@@ -5,6 +5,22 @@ import distanceBetween from '../distance-between.js'
 test('Test the distance between', async (t) => {
   await t.test('It should return the distance between 2 locations, moved 1 longitude', () => {
     const coords = {
+      latitude: 0,
+      longitude: 0
+    }
+    const location = {
+      latitude: 0,
+      longitude: 1
+    }
+
+    const result = distanceBetween({ coords, location })
+    const expected = 111194.92664455874
+
+    assert.strictEqual(result, expected)
+  })
+
+  await t.test('It should return the distance between 2 locations, moved 1 longitude on another position', () => {
+    const coords = {
       latitude: 51.5074,
       longitude: 0.1278
     }
@@ -18,7 +34,24 @@ test('Test the distance between', async (t) => {
 
     assert.strictEqual(result, expected)
   })
+
   await t.test('It should return the distance between 2 locations, moved 1 latitude', () => {
+    const coords = {
+      latitude: 0,
+      longitude: 0
+    }
+    const location = {
+      latitude: 1,
+      longitude: 0
+    }
+
+    const result = distanceBetween({ coords, location })
+    const expected = 111194.92664455874
+
+    assert.strictEqual(result, expected)
+  })
+
+  await t.test('It should return the distance between 2 locations, moved 1 latitude on another position', () => {
     const coords = {
       latitude: 51.5074,
       longitude: 0.1278
