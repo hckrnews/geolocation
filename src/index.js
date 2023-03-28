b82f5f49190f322f4afdd7aeb59a94236c0d3919
Pieter Wigboldus <pieter.wigboldus@pon.com>
2023-03-28 20:24:40 +0200

    Add files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import getLocationDistance from './get-location-distance.js'
 import filterLocations from './filter-locations.js'
+import getDistanceBetween from './helpers/distance-between.js'
 
 export {
   getLocationDistance,
-  filterLocations
+  filterLocations,
+  getDistanceBetween
 }
